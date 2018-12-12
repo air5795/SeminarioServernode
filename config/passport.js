d2e435a59d76fd1ee0bd100c3fde1b1ba187bf79
denilson-brayan-10 <futbol.deni10@gmail.com>
2018-12-12 18:32:25 -0400

    config passports user

--- config/passport.js
+++ config/passport.js
@@ -20,3 +20,20 @@ module.exports = function(passport) {
       });
   }));
 };
+module.exports = function(user ) {
+    var opts = {};
+    opts.jwtFromRequest= ExtractJwt.fromHeader('token'),
+    opts.secretOrKey = config.secret;
+    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+      User.findOne({id: jwt_payload.id}, function(err, user) {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                done(null, user);
+            } else {
+                done(null, false);
+            }
+        });
+    }));
+  };
