503fbdb8115297032ee48879440a3ca47702a117
denilson-brayan-10 <futbol.deni10@gmail.com>
2018-12-12 10:55:06 -0400

    cambios en  database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb://user:password@127.0.0.1:27017/basededatos', function(error){
+//mongoose.connectar('mongodb://user:password@127.0.0.1:27017/basededatos', function(error){
 mongoose.connect('mongodb://127.0.0.1:27017/seminario', function(error){
     if(error) console.log(error);
     console.log("connection successful");
